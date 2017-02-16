4eb5f8998c012df647488a005141b53ae6962f66
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-02-16 17:05:57 -0500

    The WAY better version ion of what I submitted last time.

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ var liri = {
 				if (err) {
 					console.log(err);
 				}	
-			});
+		})
 	},
 
 	pullTweets: function(){
@@ -41,126 +41,139 @@ var liri = {
 		  	}
 		  }
 		});
+
+	},
+
+	pullSpotify: function(request){
+		liri.write("User input: " + liri.userInput);
+	
+		// Requiring Spotify npm.
+		var spotify = require('spotify');
+
+		// 	// If the user puts in a request Spotify will pull up the first 5 matches.
+				spotify.search({ type: 'track', query: request }, function(err, data) {
+				    if ( err ) {
+				    	console.log(err);
+				    }
+					for (var i = 0; i < 5; i++) {
+						console.log("************************************************");
+						console.log("Artist: " + data.tracks.items[i].artists[0].name);
+						console.log("Song Name: " + data.tracks.items[i].name);
+						console.log("Preview Link: " + data.tracks.items[i].preview_url);
+						console.log("Album: " + data.tracks.items[i].album.name);
+						console.log(" ");
+						console.log("************************************************");
+						liri.write(" " + " Result " + [i] + " " + 
+									" Artist: " + data.tracks.items[i].artists[0].name + 
+									" Song Name: " + data.tracks.items[i].name + 
+									" Preview Link: " + data.tracks.items[i].preview_url + 
+									" Album: " + data.tracks.items[i].album.name + ""); 
+					}
+				});
+
+	},
+
+	pullImdb: function(){
+		liri.write("User input: " + liri.userInput);
+
+	// Setting variable for imdb npm and key needed to pull data.
+		var imdb = require('moviedb')('07a38162b206d7305e4adf4b96fc0fe9');
+		
+			// // If the user puts in a request IMDB will pull up the first match.
+				imdb.searchMovie({ query: liri.request }, (err, res) => {
+					if (err){
+						console.log(err); 
+			  		}
+			  		// If you want to loop through more than one result.
+					// for (var i = 0; i < 5; i++) {
+						console.log("************************************************");
+						console.log(" ");
+				  		console.log("Movie Title: " + res.results[0].original_title);
+					  	console.log("Release date: " + res.results[0].release_date);
+					  	console.log("Language: " + res.results[0].original_language);
+					  	console.log("Plot: " + res.results[0].overview);
+					  	console.log("IMDB Rating: " + res.results[0].vote_average);
+						console.log(" ");
+						console.log("************************************************");
+						liri.write(" " + " Result " + [0] + " " + 
+									" Movie Title: " + res.results[0].original_title + 
+									" Release date: " + res.results[0].release_date + 
+									" Language: " + res.results[0].original_language + 
+									" Plot: " + res.results[0].overview + 
+									" IMDB Rating: " + res.results[0].vote_average + " "); 
+					// }
+				});
 	}
 
 }
 
+// Conditionals based on user request go down here.
 if (liri.userInput === "my-tweets"){
 
-	liri.pullTweets();
+	liri.pullTweets();	
 
 } else if (liri.userInput === "spotify-this-song"){
-	
-	liri.write("User input: " + liri.userInput);
-	
-	// Requiring Spotify npm.
-	var spotify = require('spotify');
 
-// Combining Node process.argv from index 3 and beyond to create a single request.
+	// Combining Node process.argv from index 3 and beyond to create a single request.
 	for (var i=3; i < liri.nodeArgs.length; i++){
 		liri.request = liri.request + " " + liri.nodeArgs[i];
-	
-		var newRequest= liri.request;
-		
-		// If the user puts in a request Spotify will pull up the first 5 matches.
-		if (liri.request === liri.request) {
-			spotify.search({ type: 'track', query: liri.request }, function(err, data) {
-			    if ( err ) {
-			    	console.log(err);
-			    }
-				for (var i = 0; i < 5; i++) {
-					console.log("************************************************");
-					console.log("Artist: " + data.tracks.items[i].artists[0].name);
-					console.log("Song Name: " + data.tracks.items[i].name);
-					console.log("Preview Link: " + data.tracks.items[i].preview_url);
-					console.log("Album: " + data.tracks.items[i].album.name);
-					console.log(" ");
-					console.log("************************************************");
-					liri.write(" " + " Result " + [i] + " " + 
-								" Artist: " + data.tracks.items[i].artists[0].name + 
-								" Song Name: " + data.tracks.items[i].name + 
-								" Preview Link: " + data.tracks.items[i].preview_url + 
-								" Album: " + data.tracks.items[i].album.name + ""); 
-				}
-			});
-		} 
 	}
+		
+	var spotify = require('spotify');
+
 	// Calling npm to pull data specifically for the song "The Sign" from Spotify if the user does not enter a request.
 	if (liri.request === "") {
-		console.log(liri.request);
+		
+			liri.write("User input: " + liri.userInput + " Request was left blank.");
 
-		liri.write("User input: " + liri.userInput + " Request was left blank.");
+			spotify.search({type: 'track', query: 'The Sign'}, function(err, data) {
+				if (err){
+					console.log(err);
+				}
+					console.log("************************************************");
+					console.log(" ");
+					console.log("Artist: " + data.tracks.items[4].artists[0].name);
+					console.log("Song Name: " + data.tracks.items[4].name);
+					console.log("Preview Link: " + data.tracks.items[4].preview_url);
+					console.log("Album: " + data.tracks.items[4].album.name);
+					console.log(" ");
+					console.log("************************************************");
+			});
+	} else if (liri.request === liri.request) {
 
-		spotify.search({type: 'track', query: 'The Sign'}, function(err, data) {
-			if (err){
-				console.log(err);
-			}
-				console.log("************************************************");
-				console.log(" ");
-				console.log("Artist: " + data.tracks.items[4].artists[0].name);
-				console.log("Song Name: " + data.tracks.items[4].name);
-				console.log("Preview Link: " + data.tracks.items[4].preview_url);
-				console.log("Album: " + data.tracks.items[4].album.name);
-				console.log(" ");
-				console.log("************************************************");
-		});
+		liri.pullSpotify(liri.request);
 	}
-	  		
+
 }else if (liri.userInput === "movie-this"){
 
-	liri.write("User input: " + liri.userInput);
-
-// Setting variable for imdb npm and key needed to pull data.
 	var imdb = require('moviedb')('07a38162b206d7305e4adf4b96fc0fe9');
 
 	// Combining Node process.argv from index 3 and beyond to create a single request.
 	for (var i=3; i < liri.nodeArgs.length; i++){
 		liri.request = liri.request + " " + liri.nodeArgs[i];
-	
-		var newRequest= liri.request;
-		console.log(newRequest);
-
-		// If the user puts in a request IMDB will pull up the first match.
-		if (liri.request === newRequest){
-			imdb.searchMovie({ query: liri.request }, (err, res) => {
-				if (err){
-					console.log(err); 
-		  		}
-		  		// If you want to loop through more than one result.
-				// for (var i = 0; i < 5; i++) {
-					console.log("************************************************");
-					console.log(" ");
-			  		console.log("Movie Title: " + res.results[0].original_title);
-				  	console.log("Release date: " + res.results[0].release_date);
-				  	console.log("Language: " + res.results[0].original_language);
-				  	console.log("Plot: " + res.results[0].overview);
-				  	console.log("IMDB Rating: " + res.results[0].vote_average);
-					console.log(" ");
-					console.log("************************************************");
-					liri.write(" " + " Result " + [0] + " " + 
-								" Movie Title: " + res.results[0].original_title + 
-								" Release date: " + res.results[0].release_date + 
-								" Language: " + res.results[0].original_language + 
-								" Plot: " + res.results[0].overview + 
-								" IMDB Rating: " + res.results[0].vote_average + " "); 
-				// }
-			});
-		}
 	}
+
 	// Calling npm to pull data specifically for the movie "Mr. Nobody" from IMDB if the user does not enter a request.
-	if (liri.request === "") {
-		console.log(liri.request);
-	
-		liri.write("User input: " + liri.userInput + " Request was left blank.");
+		if (liri.request === "") {
+		
+			liri.write("User input: " + liri.userInput + " Request was left blank.");
 
-		imdb.searchMovie({ query: "Mr. Nobody" }, (err, res) => {
-			console.log("Movie Title: " + res.results[0].original_title);
-			console.log("Release date: " + res.results[0].release_date);
-			console.log("Language: " + res.results[0].original_language);
-			console.log("Plot: " + res.results[0].overview);
-			console.log("IMDB Rating: " + res.results[0].vote_average);
-		});
-	}
+			imdb.searchMovie({ query: "Mr. Nobody" }, (err, res) => {
+				console.log("************************************************");
+				console.log(" ");
+				console.log("Movie Title: " + res.results[0].original_title);
+				console.log("Release date: " + res.results[0].release_date);
+				console.log("Language: " + res.results[0].original_language);
+				console.log("Plot: " + res.results[0].overview);
+				console.log("IMDB Rating: " + res.results[0].vote_average);
+				console.log(" ");
+				console.log("************************************************");
+
+			});
+		} else if (liri.request === liri.request) {
+
+			liri.pullImdb(liri.request);
+		}
 
 } else if (liri.userInput === "do-what-it-says"){
 	
@@ -174,31 +187,9 @@ if (liri.userInput === "my-tweets"){
 		}
 		var random = data.split(",");
 // The song title is index one when we split the data from the document and turn it into an array.
-		request = random[1];
+		console.log("You've asked Liri to " + random[0] + random[1]);
+		console.log("Pulling song data now.");
 
-		var spotify = require('spotify');
-
- // Spotify will pull up the first 5 matches for the song request.
-		spotify.search({ type: 'track', query: request }, function(err, data) {
-		    if ( err ) {
-		        return;
-		    } 
-			for (var i = 0; i < 5; i++) {
-			    console.log("************************************************");
-				console.log(" ");
-				console.log("Artist: " + data.tracks.items[i].artists[0].name);
-				console.log("Song Name: " + data.tracks.items[i].name);
-				console.log("Preview Link: " + data.tracks.items[i].preview_url);
-				console.log("Album: " + data.tracks.items[i].album.name);
-				console.log(" ");
-				console.log("************************************************");
-				liri.write(" " + " Result " + [i] + " " + 
-							" Artist: " + data.tracks.items[i].artists[0].name + 
-							" Song Name: " + data.tracks.items[i].name + 
-							" Preview Link: " + data.tracks.items[i].preview_url + 
-							" Album: " + data.tracks.items[i].album.name + "");
-			}
-	 	});
+		liri.pullSpotify(random[1]);
 	});
 }
- 
